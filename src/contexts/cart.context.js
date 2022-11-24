67729ac3f855dd36ba45b0cc426ca1be263ac651
Byron Keet <keetbis@gmail.com>
2022-11-24 12:23:28 +0200

    Added functionality to cart context and cart dropdown to add items to the cart and show in the list

--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -1,16 +1,44 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+	const existingCartItem = cartItems.find(
+		(cartItem) => cartItem.id === productToAdd.id
+	);
+	
+	  if (existingCartItem) {
+		return cartItems.map((cartItem) =>
+			cartItem.id === productToAdd.id
+			? { ...cartItem, quantity: cartItem.quantity + 1 }
+			: cartItem
+		);
+	}
+	
+	  return [...cartItems, { ...productToAdd, quantity: 1 }];
+}
 
 export const CartContext = createContext({
 	isCartOpen: false,
-	setIsCartItems: () => {},
+	setIsCartOpen: () => {},
 	cartItems: [],
-	setCartItems: () => {}
+	addItemToCart: () => {},
+	cartCount: 0
 });
 
 export const CartProvider = ({ children }) => {
 	const [isCartOpen, setIsCartOpen] = useState(false);
 	const [cartItems, setCartItems] = useState([]);
-	const value = { isCartOpen, setIsCartOpen, cartItems, setCartItems };
+	const [cartCount, setCartCount] = useState(0);
+
+	useEffect(() => {
+		const newCartCount = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+		setCartCount(newCartCount);
+	}, [cartItems])
+
+	const addItemToCart = (productToAdd) => {
+		setCartItems(addCartItem(cartItems, productToAdd));
+	}
+
+	const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount };
 
 	return <CartContext.Provider value={value} >{children}</CartContext.Provider>
 };
