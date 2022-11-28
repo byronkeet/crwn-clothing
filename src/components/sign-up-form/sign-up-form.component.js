33d1deb2e8ae16693e64b25ddda4f7c5fad24855
Byron Keet <keetbis@gmail.com>
2022-11-28 14:42:00 +0200

    Added styled components library and converted all scss to js styled components

--- src/components/sign-up-form/sign-up-form.component.js
+++ src/components/sign-up-form/sign-up-form.component.js
@@ -5,7 +5,7 @@ import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
 
-import './sign-up-form.styles.scss';
+import { SignUpContainer } from './sign-up-form.styles';
 
 const defaultFormFields = {
 	displayName: '',
@@ -51,7 +51,7 @@ const SignUpForm = () => {
 		
 	}
 	return(
-		<div className='sign-up-container'>
+		<SignUpContainer>
 			<h2>Don't have an account?</h2>
 			<span>Sign up with your email and password</span>
 			<form onSubmit={handleSubmit} >
@@ -89,7 +89,7 @@ const SignUpForm = () => {
 				/>
 				<Button type='submit'>Sign Up</Button>
 			</form>
-		</div>
+		</SignUpContainer>
 	)
 }
 
