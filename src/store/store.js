4fd71d42e8eba11082d3aac6261c8cc049938974
Byron Keet <keetbis@gmail.com>
2022-11-30 07:30:15 +0200

    Add redux persist for local storage and persistance to carry state across multiple sessions

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 // import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
@@ -15,11 +17,21 @@ const loggerMiddleware = (store) => (next) => (action) => {
 	next(action);
 
 	console.log('nextState: ', store.getState());
+};
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	blacklist: ['user']
 }
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // const middleWares = [logger];
 const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
