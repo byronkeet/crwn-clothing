bf3ea688cc31fc86b713139ea8fb9435fe93bcda
Byron Keet <keetbis@gmail.com>
2022-12-01 10:43:58 +0200

    setting up netlify function and stripe payment processing with card element

--- src/routes/checkout/checkout.component.js
+++ src/routes/checkout/checkout.component.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 import { selectCartTotal, selectCartItems } from "../../store/cart/cart.selector";
 
 import {
@@ -34,6 +35,7 @@ const Checkout = () => {
 			</CheckoutHeader>
 			{ cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)}
 			<Total>Total: ${cartTotal}</Total>
+			<PaymentForm/>
 		</CheckoutContainer>
 	);
 };
